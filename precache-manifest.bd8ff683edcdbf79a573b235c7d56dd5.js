37a406ef4bd829dfe94521e74697ff5675dc9be0
Ariel Elbahar <42386117+ArielLB@users.noreply.github.com>
2020-04-22 19:34:08 +0300

    Updates

---
+++ precache-manifest.bd8ff683edcdbf79a573b235c7d56dd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875d1060a7529f9284fae6719f6f78af",
+    "url": "/ColorApp/index.html"
+  },
+  {
+    "revision": "0336c9624e6ec50914d1",
+    "url": "/ColorApp/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "9b4b86dd2224f2d2f309",
+    "url": "/ColorApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0336c9624e6ec50914d1",
+    "url": "/ColorApp/static/js/2.e2866156.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/ColorApp/static/js/2.e2866156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4b86dd2224f2d2f309",
+    "url": "/ColorApp/static/js/main.4b14458d.chunk.js"
+  },
+  {
+    "revision": "9d59ea818ec28557e367",
+    "url": "/ColorApp/static/js/runtime-main.7c1ee8e5.js"
+  },
+  {
+    "revision": "4d51f9e6fa630aa913846251bf03c717",
+    "url": "/ColorApp/static/media/background.4d51f9e6.svg"
+  }
+]);
